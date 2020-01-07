944b37fa06a44b042ab897640b9b9905760ef768
ericy <ericy@stud.ntnu.no>
2020-01-07 15:00:49 +0100

    Added filestructure and template for react /klient

---
+++ application/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Harmoni
+    </div>
+  );
+}
+
+export default App;
