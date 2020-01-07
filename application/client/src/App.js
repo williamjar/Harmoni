05a5b8af4493ac09f013ef80aa5159d0141ca4e9
ericy <ericy@stud.ntnu.no>
2020-01-07 16:04:17 +0100

    some prototype layout for menu and content

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavBar} from "./components/menu/navigation";
 
 
 
 function App() {
   return (
     <div className="App">
-      Harmoni
+      <div className="row">
+          <div className="col-lg-2">
+          <NavBar />
+          </div>
+
+
+          <div className="col-lg-9">
+              content
+          </div>
+
+
+
+
+
+      </div>
+
     </div>
   );
 }
