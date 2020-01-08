48f5c14c7d11472d1dc208fdf23040fb0885282e
andinel <andine.luick@gmail.com>
2020-01-08 15:08:58 +0100

    added dao events

---
+++ application/server/dao/artistDao.js
@@ -0,0 +1,31 @@
+const Dao = require('./dao.js');
+
+module.exports = class artistDao extends Dao {
+
+    getAll(callback) {
+        super.query('SELECT * FROM artist AND contact WHERE artist.contactID = contact.contactID', [], callback);
+    }
+
+    getOne(callback, artistID) {
+        super.query('SELECT * FROM artist WHERE artistID = ? ', [artistID], callback);
+    }
+
+    createOne(callback) {
+        super.query('INSERT INTO artist ', callback);
+    }
+
+    updateOne(json, callback){
+        var val = [json.artistID, json.artistName];
+        super.query('UPDATE artist set , where artistID = ?', callback);
+    }
+
+    deleteOne(callback, artistID) {
+        super.query('DELETE FROM ', [artistID], callback);
+    }
+
+    addDocument(callback, artistID, documentID) {
+        super.query('INSERT INTO event', [artistID], [documentID], callback);
+    }
+
+
+}
--- application/server/dao/riderDao.js
+++ application/server/dao/riderDao.js
@@ -2,4 +2,8 @@ const Dao = require('./dao.js');
 
 module.exports = class riderDao extends Dao {
 
+    createOne(callback) {
+        super.query('INSERT INTO riderElement ', callback);
+    }
+
 };
