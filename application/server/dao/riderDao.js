ba3b7dfb86772701be6c286e3a617983adb3f601
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-08 15:09:30 +0100

    added updateOne to riderDao

--- application/server/dao/riderDao.js
+++ application/server/dao/riderDao.js
@@ -1,5 +1,7 @@
 const Dao = require('./dao.js');
 
 module.exports = class riderDao extends Dao {
-
+    updateOne(callback, riderElementID, artistID, eventID) {
+        super.query('UPDATE ', [riderElementID, artistID, eventID], callback);
+    }
 };
