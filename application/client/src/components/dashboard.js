d70cd4d766fd850a0313f040f56d09088c546fcc
marcuse <marcuse@stud.ntnu.no>
2020-01-08 08:44:33 +0100

    created new branch, added dashboard.js

---
+++ application/client/src/components/dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export class Dashboard extends React.Component {
+
+    render() {
+
+        return(
+            <Table striped>
+                <tbody>
+
+                </tbody>
+            </Table>
+        )
+    }
+}
