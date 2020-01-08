03842e2efb8c883e84b060dbb6e432c8e2daf10b
jonaslarsson <jonasbl>
2020-01-08 15:49:19 +0100

    Working on the add ticket GUI.

---
+++ application/client/src/components/ticket.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
+
+
+/* Component to add tickets to concert*/
+export class AddTicket extends Component{
+
+    saveTicket = event =>{
+        //this.ticket = event.target.value
+        console.log('Ticket Saved')
+    };
+
+
+    render() {
+        return (
+            <Accordion>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                            <Button variant="primary" size="sm">
+                                + Lag ny billett
+                            </Button>
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            <Form>
+                                <Form.Row className="ticketStyle">
+                                    <Col sm={2}>
+                                        <label>Navn billett</label>
+                                        <Form.Control placeholder="Navn"/>
+                                    </Col>
+                                    <Col sm={1}>
+                                        <label>Pris</label>
+                                        <Form.Control placeholder="kr,-"/>
+                                    </Col>
+                                    <Col sm={1}>
+                                        <label>Antall billetter</label>
+                                        <Form.Control placeholder="Antall"/>
+                                    </Col>
+                                    <Col sm={1}>
+                                        <label>Slipp dato</label>
+                                        <Form.Control placeholder="10/10/20"
+                                        onChange={this.saveTicket}/>
+                                    </Col>
+                                    <Col sm={1}>
+                                        <label>Klokkeslett</label>
+                                        <Form.Control placeholder="14:00"/>
+                                    </Col>
+                                    <Col>
+                                        <Form.Check
+                                            type="switch"
+                                            id="custom-switch"
+                                            label="Slutt dato?"
+                                        />
+                                    </Col>
+                                </Form.Row>
+                                <Form.Row className="ticketStyle">
+                                <Col sm={6}>
+                                    <Form.Control placeholder="Beskrivelse"/>
+                                </Col>
+                            </Form.Row>
+                                <Button variant="primary" size="sm" onClick={this.saveTicket}>
+                                    Lagre billett
+                                </Button>
+                            </Form>
+
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+
+        );
+    }
+}
--- application/client/src/index.js
+++ application/client/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from "./App";
+import {AddTicket, Ticket} from "./components/ticket";
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<AddTicket/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
