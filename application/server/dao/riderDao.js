6c48daf43347b52cc6740dd2d50adc0442646193
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-08 15:59:59 +0100

    Added SQL statement to rider Dao

--- application/server/dao/riderDao.js
+++ application/server/dao/riderDao.js
@@ -5,9 +5,10 @@ module.exports = class riderDao extends Dao {
     //TODO: Write SQL statements
 
     createOne(callback, artistID, eventID) {
-        super.query('INSERT INTO riderElement ', [artistID, eventID] , callback);
+        super.query('INSERT INTO riderElement VALUES (default, ?, ?, default, 0, default) ', [artistID, eventID] , callback);
     }
 
+    //TODO: Needs more parameters
     updateOne(callback, riderElementID, artistID, eventID) {
         super.query('UPDATE ', [riderElementID, artistID, eventID], callback);
     }
