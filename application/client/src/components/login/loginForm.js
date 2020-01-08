f410e33f5fe241dd9aa6af2e4a854f9e19fb02ae
williamjar <williaj@stud.ntnu.no>
2020-01-08 12:30:30 +0100

    Updated with feedback if the password or username is not correct.

--- application/client/src/components/login/loginForm.js
+++ application/client/src/components/login/loginForm.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import {Form, Button, Card} from 'react-bootstrap'
+import {Form, Button, Card,Col} from 'react-bootstrap'
 
 
 
@@ -11,7 +11,8 @@ export class LoginForm extends React.Component {
         super(props);
         this.state = {
             email : '',
-            password : ''
+            password : '',
+            flag : 1
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -34,39 +35,53 @@ export class LoginForm extends React.Component {
     }
 
     render(): React.Node {
+        let infoText;
+        if(this.state.flag === -1){
+            infoText = <Form.Text className="text-danger"> Feil brukernavn eller passord, vennligst prøv igjen</Form.Text>;
+        } else {
+            infoText =  <Form.Text className="text-danger"></Form.Text>;
+        }
         return (
 
-            <Card className="m-4">
-            <div className="card-header"><h2 className="card-title">Logg inn</h2></div>
-            <div class="m-4">
-            <Form onSubmit={this.handleSubmit}>
+            <Card className="m-5">
+                <div className="card-header"><h2 className="card-title">Logg inn</h2></div>
+                <Col sm={4}>
+                    <div class="m-5">
+                        <Form onSubmit={this.handleSubmit}>
 
-            <Form.Group>
-            <Form.Control type="email" name="email" placeholder="Epostaddresse" value={this.state.email} onChange={this.handleInputChange}/>
-            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="email" name="email" placeholder="E-postadresse" value={this.state.email} onChange={this.handleInputChange}/>
+                            </Form.Group>
 
-            <Form.Group>
-            <Form.Control type="password" maxLength="30" name="password" placeholder="Passord" value={this.state.password} onChange={this.handleInputChange}/>
-            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="password" maxLength="30" name="password" placeholder="Passord" value={this.state.password} onChange={this.handleInputChange}/>
+                            </Form.Group>
 
-            <Button variant="btn btn-primary btn-lg" type="submit"> Logg inn </Button>
+                            <Button variant="btn btn-primary btn-lg" type="submit"> Logg inn </Button>
 
-            <Form.Text> Ny bruker? Klikk her for registrere deg</Form.Text>
+                            <Form.Text> Ny bruker? Klikk her for registrere deg</Form.Text>
 
-            </Form>
-            </div>
+                            {infoText}
+                        </Form>
+                    </div>
+                </Col>
             </Card>
 
-    )
+        )
+    }
+    inCorrectLoginFeedback(){
+
     }
 
     submitTicket(){
+        this.setState({ flag: -1 });
+        alert("Form submitted." + "\n" + "Username: " + "\n" + this.state.email + "\n" + "Password:"+  "\n" + this.state.password);
 
-
+        /*
+        *   Service code goes here. The login variables(email, password) can be accessed via the state variables "this.state.email" and "this.state.password";
+        * */
     }
 
-    incorrectPasswordFeedback() {
 
-    }
 
 }
