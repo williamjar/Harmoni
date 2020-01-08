3572de3c4a86c52c3976006739f28df96e901a7e
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-08 14:28:22 +0100

    Added DAO methods to eventDao

--- application/server/dao/eventDao.js
+++ application/server/dao/eventDao.js
@@ -10,11 +10,43 @@ module.exports = class eventDao extends Dao {
         super.query('SELECT ', [eventID], callback);
     }
 
-    getbyStatus(callback, eventID) {
+    getByStatus(callback, eventID) {
         super.query('SELECT ', [eventID], callback);
     }
 
     deleteOne(callback, eventID) {
         super.query('DELETE FROM ', [eventID], callback);
     }
+
+    publishOne(callback, eventID) {
+        super.query('UPDATE  ', [eventID], callback);
+    }
+
+    ArchiveOne(callback, eventID) {
+        super.query('UPDATE ', [eventID], callback);
+    }
+
+    getAllPublished(callback) {
+        super.query('SELECT ', [], callback);
+    }
+
+    getAllPlanned(callback) {
+        super.query('DELETE FROM ', [], callback);
+    }
+
+    getAllArchived(callback){
+        super.query('SELECT', [], callback);
+    }
+
+    getNumberArchived(callback) {
+        super.query('SELECT COUNT(*) FROM', [], callback);
+    }
+
+    getXArchived(callback, x){
+        super.query('SELECT ', [x], callback);
+    }
+
+
+
+
 }
