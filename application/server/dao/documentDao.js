92a9ed2983658e6d59336e78930e68de4353cff3
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-08 15:46:26 +0100

    added Dao methods to documentDao

--- application/server/dao/documentDao.js
+++ application/server/dao/documentDao.js
@@ -2,6 +2,15 @@ const Dao = require('./dao.js');
 
 module.exports = class documentDao extends Dao {
 
+    createOne(callback) {
+        super.query('INSERT INTO document ', [], callback);
+    }
 
+    updateOne(callback, documentID) {
+        super.query('UPDATE artist ', [documentID], callback);
+    }
 
+    deleteOne(callback, documentID) {
+        super.query('DELETE FROM ', [documentID], callback);
+    }
 };
