b3dfb36fa25ab3cb18ddf09fa284c56ea2f2ffc0
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-01-08 13:55:49 +0100

    Dao branch, created dao.js

---
+++ application/server/dao/dao.js
@@ -0,0 +1,26 @@
+module.exports = class Dao {
+    constructor(pool) {
+        this.pool = pool;
+    }
+    query(sql, params, callback) {
+        this.pool.getConnection((err, connection) => {
+            console.log("dao: connected to database");
+            if (err) {
+                console.log("dao: connection error");
+                callback(500, { error: "connection error" });
+            } else {
+                console.log("dao: running sql: " + sql);
+                connection.query(sql, params, (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                        callback(500, { error: "querying error" });
+                    } else {
+                        console.log("dao: returning rows");
+                        callback(200, rows);
+                    }
+                });
+            }
+        });
+    }
+};
