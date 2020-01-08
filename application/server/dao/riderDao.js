74748ab93d7502f713137156a901a46f2801493d
andinel <andine.luick@gmail.com>
2020-01-08 15:41:28 +0100

    updated dao events

--- application/server/dao/riderDao.js
+++ application/server/dao/riderDao.js
@@ -2,8 +2,8 @@ const Dao = require('./dao.js');
 
 module.exports = class riderDao extends Dao {
 
-    createOne(callback) {
-        super.query('INSERT INTO riderElement ', callback);
+    createOne(callback, artistID, eventID) {
+        super.query('INSERT INTO riderElement ', [artistID, eventID] , callback);
     }
 
     updateOne(callback, riderElementID, artistID, eventID) {
