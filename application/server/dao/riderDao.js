45d9e663394577e70720b41f409968bd8e755e56
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-09 09:01:16 +0100

    Finished writing SQL statements in riderDao

--- application/server/dao/riderDao.js
+++ application/server/dao/riderDao.js
@@ -2,14 +2,15 @@ const Dao = require('./dao.js');
 
 module.exports = class riderDao extends Dao {
 
-    //TODO: Write SQL statements
-
     createOne(callback, artistID, eventID) {
         super.query('INSERT INTO riderElement VALUES (default, ?, ?, default, 0, default) ', [artistID, eventID] , callback);
     }
 
-    //TODO: Needs more parameters
-    updateOne(callback, riderElementID, artistID, eventID) {
-        super.query('UPDATE ', [riderElementID, artistID, eventID], callback);
+    updateOne(callback, riderElement) {
+        super.query('UPDATE riderElement SET status = ?, isDone = ?, description = ? WHERE riderElementID = ?' , [riderElement.status, riderElement.isDone, riderElement.description, riderElement.riderID], callback);
+    }
+
+    deleteOne(callback, riderElementID) {
+        super.query('DELETE FROM riderElement WHERE riderElementID = ?', [riderElementID], callback);
     }
 };
