efdd96e7b804e457679652054fd6f006e0b8c3bf
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-09 09:28:04 +0100

    Added category daos in crewDao

--- application/server/dao/crewDao.js
+++ application/server/dao/crewDao.js
@@ -20,11 +20,20 @@ module.exports = class crewDao extends Dao {
         super.query('SELECT * FROM crew, contact, event_crewCategory_crew WHERE eventID = ? AND crew.crewID = event_crewCategory_crew.crewID AND crew.contactID = contact.contactID', [eventID], callback);
     }
 
-    addDocument(callback, documentLink, crewID, documentCategoryID) {
-        super.query('INSERT INTO document (documentLink, crewID, documentCategoryID) ', [documentLink, crewID, documentCategoryID], callback);
+    addDocument(callback, crewID, documentID) {
+        super.query('INSERT INTO document VALUES (DEFAULT, ?, ?, ?, ?, NULL, ?, documentCategoryID)', [crewID, documentID], callback);
     }
 
-    setResponsible(callback, crewID, isResponsible){
-        super.query('UPDATE event_crewCategory_crew set isResponsible = ? WHERE crewID = ? ', [crewID, isResponsible], callback);
+    setResponsible(callback, eventID, crewID, crewCategoryID, responsible){
+        super.query('UPDATE event_crewCategory_crew SET isResponsible = ? WHERE eventID = ? AND crewCategoryID = ? AND crewID = ?', [responsible, eventID, crewCategoryID, crewID], callback);
     }
+
+    getAllCategories(callback){
+        super.query('SELECT * FROM crewCategory', [], callback);
+    }
+
+    createOneCategory(callback, crewCategoryName, organizerID){
+        super.query('INSERT INTO crewCategory VALUES (DEFAULT, ?, ?)', [crewCategoryName, organizerID], callback);
+    }
+
 };
