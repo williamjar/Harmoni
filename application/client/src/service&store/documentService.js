f6875e026d7ba7c551f68e3ae8359042060b64d0
andinel <andine.luick@gmail.com>
2020-01-10 15:25:49 +0100

    updated document services

--- application/client/src/service&store/documentService.js
+++ application/client/src/service&store/documentService.js
@@ -4,23 +4,29 @@ import Document from ".classes/document.js"
 
 class DocumentService {
 
-    getAllDocumentsForOrganizer(organizerID: number) {
+    getAllDocumentsForOrganizer(organizerID) {
         return axios.get<Document[]>('/api/organizer/' + organizerID + '/documents').then(response => response.data);
     }
 
-    getAllDocumentsForEvent(eventID: number){
+    getAllDocumentsForEvent(eventID){
         return axios.get<Document[]>('/api/events/' + eventID + '/documents').then(response => response.data);
     }
 
-    addDocument(){
-        return axios.post<Document, void>('/api/document/', document).then(response => response.data);
+    addDocument(eventID, documentLink, artistID, documentCategoryID){
+        axios.post('/api/document/', {
+            "eventID": eventID,
+            "documentLink": documentLink,
+            "artistID": artistID,
+            "documentCategoryID" : documentCategoryID
+    }).then(response => response.data);
     }
 
-    updateDocument(document: Document) {
+
+    updateDocument(document) {
         return axios.put<Document, void>('/api/document/' + id, document).then(response => response.data);
     }
 
-    deleteDocument(id: number) {
+    deleteDocument(id) {
         return axios.delete<Document>('/api/document/' + id).then(response => response.data);
     }
 
