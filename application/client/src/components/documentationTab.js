e6274abda788aa826a45ba2ecd0b0fb557537e1d
jonaslarsson <jonasbl>
2020-01-10 10:24:27 +0100

    Dropzone only accepts documentation in pdf format.

--- application/client/src/components/documentationTab.js
+++ application/client/src/components/documentationTab.js
@@ -3,39 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from "react-bootstrap/Card";
 import Dropzone from 'react-dropzone';
 import { FaArrowAltCircleDown } from 'react-icons/fa';
-import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
-
-
-
 
 export class DocumentationTab extends Component{
 
-
+    //Prints out the adde files in the console log.
     onDrop(acceptedFiles){
         console.log(acceptedFiles);
     };
 
+    //Renders a dropzone to drop files.
     render() {
         return (
             <Card.Body>
                 <h3>Legg til dokumnter</h3>
-                <div className="dropzoneForm">
-                    <Form>
-                        <Dropzone onDrop={this.onDrop} className="dropzone">
+                <div className="dropzone">
+                        <Dropzone
+                            onDrop={this.onDrop}
+                            accept="application/pdf"
+                        >
                             {({getRootProps, getInputProps}) => (
                                 <div {...getRootProps()}>
                                     <input  {...getInputProps()} />
+
                                     <h1><FaArrowAltCircleDown/></h1>
                                     Legg til filer her!
                                 </div>
                             )}
                         </Dropzone>
-                    </Form>
                 </div>
-
-
-
             </Card.Body>
         );
     }
