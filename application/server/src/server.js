9b2d7bb1baa93e0051ca5d6ed8d00519ceec9c48
Jørgen Hollum <jorho@stud.ntnu.no>
2020-01-13 16:08:53 +0100

    Picture class and service added

--- application/server/src/server.js
+++ application/server/src/server.js
@@ -21,9 +21,9 @@ app.use(cors());
 const pool = mysql.createPool({
     connectionLimit: 2,
     host: "mysql.stud.iie.ntnu.no",
-    user: "evengu",
-    password: "O7KhlwWQ",
-    database: "evengu",
+    user: "joakimad",
+    password: "LQliMP1A",
+    database: "joakimad",
     debug: false,
     multipleStatements: true
 });
@@ -38,6 +38,7 @@ const organizerDaoObj = require('./dao/organizerDao.js');
 const riderDaoObj = require('./dao/riderDao.js');
 const documentationDaoObj = require("./dao/documentationdao.js");
 const loginDaoObj = require("./dao/loginDao");
+const pictureDaoObj = require("./dao/pictureDao");
 let artistDao = new artistDaoObj(pool);
 let bugDao = new bugDaoObj(pool);
 let contactDao = new contactDaoObj(pool);
@@ -48,6 +49,7 @@ let eventDao = new eventDaoObj(pool);
 let organizerDao = new organizerDaoObj(pool);
 let riderDao = new riderDaoObj(pool);
 let loginDao = new loginDaoObj(pool);
+let pictureDao = new pictureDaoObj(pool);
 
 const public_path = path.join(__dirname, '/../../client/public');
 app.use(express.static(public_path));
@@ -1012,9 +1014,41 @@ app.delete("/api/document/:documentID", (request, response) => {
 });
 
 // PICTURE
-//Delete
-//Update
-//Insert
+//Insert picture
+app.post("/api/picture/insert", (request, response) => {
+    console.log("Request to add a picture link");
+    pictureDao.createOne((status, data) => {
+        response.status(status);
+        response.json(data);
+    }, request.body.pictureLink);
+});
+
+//Delete picture
+app.delete("/api/picture/delete/:pictureID", (request, response) => {
+    console.log("Request to delete a picture");
+    pictureDao.deleteOne((status, data) => {
+        response.status(status);
+        response.json(data);
+    }, request.params.pictureID);
+});
+
+//Update picture
+app.put("/api/picture/update/:pictureID", (request, response) => {
+    console.log("Request to update a picture");
+    pictureDao.updateOne((status, data) => {
+        response.status(status);
+        response.json(data);
+    }, request.body.pictureLink, request.params.pictureID);
+});
+
+//Get one picture
+app.get("/api/picture/:pictureID", (require, response) => {
+    console.log("Request to get a rider element");
+    pictureDao.getPicture((status, data) => {
+        response.status(status);
+        response.json(data);
+    }, require.params.pictureID);
+});
 
 
 const server = app.listen(8080);
