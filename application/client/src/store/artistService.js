9746b6fb13c68d2c96f9a8705b229cf51bb4a9fc
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-14 12:12:17 +0100

    changed contactNAme to username in contactBody for createArtist

--- application/client/src/store/artistService.js
+++ application/client/src/store/artistService.js
@@ -31,7 +31,7 @@ export class ArtistService {
         };
 
         let contactBody = {
-            "contactName": name,
+            "username": name,
             "phone": phone,
             "email": email
         };
