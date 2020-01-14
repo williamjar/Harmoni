cb0b9d127382411b1292a26d0db1679295bef408
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-14 10:21:08 +0100

    small fix in createEvent in eventStore, organizerID was in the wrong parameter slot

--- application/client/src/store/eventStore.js
+++ application/client/src/store/eventStore.js
@@ -25,7 +25,7 @@ export class eventStore{
         axios.post(axiosConfig.root + "/api/events" , {headers: header}).then(response =>{
 
             //Create an event from the insertID returned from the query and the organizerID, the rest is null
-            this.currentEvent = new Event(response.data.insertId, null, null, null, null, null, null, null, null, null, null, null, null, null, organizerID, null);
+            this.currentEvent = new Event(response.data.insertId, null, null, null, null, null, null, null, null, null, null, null, null, organizerID, null);
             callback();
         });
 
