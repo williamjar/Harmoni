7d9c1fec9f148d994828c4fd2b6f80cf5feea5bd
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-14 10:41:38 +0100

    Updated //Create one event in server.js, the endpoint was missing the organizerID

--- application/server/src/server.js
+++ application/server/src/server.js
@@ -734,7 +734,7 @@ app.post("/api/events", (request, response) => {
     eventDao.createOne((status, data) => {
         response.status(status);
         response.json(data);
-    }, [request.body.eventName, request.body.startDate, request.body.endDate, request.body.startTime, request.body.endTime, request.body.address, request.body.town, request.body.zipCode, request.body.status, request.body.description, request.body.publishDate, request.body.publishTime, request.body.eventTypeID, request.body.pictureID]);
+    }, [request.body.eventName, request.body.startDate, request.body.endDate, request.body.startTime, request.body.endTime, request.body.address, request.body.town, request.body.zipCode, request.body.status, request.body.description, request.body.publishDate, request.body.publishTime, request.body.organizerID, request.body.eventTypeID, request.body.pictureID]);
 });
 
 //Update event
