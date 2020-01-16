73431fb6e60925b67c1c1b9b85cbf829b310a678
marcuse <marcuse@stud.ntnu.no>
2020-01-16 12:44:00 +0100

    cleaned and commented code in dashboard components

--- application/client/src/components/content/dashboard/eventView.js
+++ application/client/src/components/content/dashboard/eventView.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from "react-bootstrap/Table";
 import {EventCard} from "./eventCard";
 
+// Component for displaying a group of events(EventCard's) as a table
 export class EventView extends React.Component {
 
     state = {
         events: [this.props.events]
     };
 
+    // Updates the state if the received props from parent is changed
     static getDerivedStateFromProps(props, state) {
         if(props.events !== state.events) {
             return {
@@ -20,9 +22,6 @@ export class EventView extends React.Component {
     }
 
     render() {
-
-        console.log(this.state.events);
-
         return(
             <Table responsive>
                 <tbody>
