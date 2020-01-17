14a61924799940dcb778ac6fd9db1ebb15c73294
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-01-17 10:21:55 +0100

    implemented delete button.

--- application/client/src/components/content/tabContent.js
+++ application/client/src/components/content/tabContent.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Col, Row} from "react-bootstrap";
+import {EventStore} from "../../store/eventStore";
+import {createHashHistory} from "history";
+
+const history = createHashHistory();
 
 // Container-component for the different tabs in EventForm
 // Renders differently when editing events
@@ -10,15 +14,17 @@ export class TabContent extends Component {
         editable: [this.props.editable],
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="tabContent">
                 <div className="tabChildren">
                     {this.props.children}
                 </div>
                 <Row>
                     <Col xs={8}>
-                        {this.state.editable ? <Button className="mr-1" variant="secondary" onClick={this.props.saveClicked}>Lagre og lukk</Button> :
+                        {this.state.editable ?
+                            <Button className="mr-1" variant="secondary" onClick={this.props.saveClicked}>Lagre og
+                                lukk</Button> :
                             <Button onClick={this.props.editClicked}>Rediger</Button>}
                     </Col>
                     <Col xs={6} md={3}>
@@ -27,8 +33,8 @@ export class TabContent extends Component {
                                 <div>
                                     <Button className="mr-1" onClick={this.props.onClick}>Neste</Button>
 
-                                    <Button variant="danger">Slett arrangement</Button>
-                                    <Button  className="mr-1" disabled variant="success">Publiser</Button>
+                                    <Button variant="danger" onClick={this.deleteClicked}>Slett arrangement</Button>
+                                    <Button className="mr-1" disabled variant="success">Publiser</Button>
                                 </div> : null
                         }
                     </Col>
@@ -40,11 +46,17 @@ export class TabContent extends Component {
     // Updates the state when the received props from parent changes
     static getDerivedStateFromProps(props, state) {
 
-        if(props.editable !== state.editable) {
+        if (props.editable !== state.editable) {
             return {
                 editable: props.editable
             };
         }
         return null;
     }
+
+    deleteClicked = () => {
+        console.log("DELETE BUTTON CLICKED!");
+        EventStore.deleteCurrentEvent().then(console.log('Event deleted'));
+        history.push("/");
+    };
 }
