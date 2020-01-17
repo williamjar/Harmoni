c44fb066d854a69e8b15ae61db0680864005fe11
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-01-17 14:17:22 +0100

    updated dashboard categories and a foreign key for deleteing events

--- application/client/src/components/content/dashboard/dashboard.js
+++ application/client/src/components/content/dashboard/dashboard.js
@@ -25,7 +25,7 @@ const history = createHashHistory();
 // Component displaying all of the users events
 export class Dashboard extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -38,16 +38,15 @@ export class Dashboard extends React.Component {
     filterEvents = (e) => {
         this.setState({active: e.target.name});
 
-        if(this.state.active === "all") {
+        if (this.state.active === "all") {
 
-        }
-        else if(this.state.active === "planned") {
+        } else if (this.state.active === "planning") {
 
-        }
-        else if(this.state.active === "planning") {
+        } else if (this.state.active === "published") {
 
-        }
-        else if(this.state.active === "archived") {
+        } else if (this.state.active === "archived") {
+
+        } else if (this.state.active === "cancelled") {
 
         }
     };
@@ -59,35 +58,43 @@ export class Dashboard extends React.Component {
 
     // Stores all the organizer's events before rendering the page
     componentDidMount() {
-        EventStore.storeAllEventsForOrganizer(() => {this.setState({events: EventStore.allEventsForOrganizer})}, CookieStore.currentUserID);
+        EventStore.storeAllEventsForOrganizer(() => {
+            this.setState({events: EventStore.allEventsForOrganizer})
+        }, CookieStore.currentUserID);
     }
 
     render() {
 
-        return(
+        return (
             <Card className={"border-0 justify-content-md-center m-4"}>
                 <h3 className={"mt-4 mb-4"}>Arrangementer</h3>
                 <Search searchHandler={this.searchHandler}/>
                 <Row className="filterMenu">
-                        <Col>
-                            <ButtonGroup size="sm">
-                                <Button name="all" variant="secondary" active={this.state.active === "all"} onClick={this.filterEvents}>Alle</Button>
-                                <Button name="planned" variant="secondary" active={this.state.active === "planned"} onClick={this.filterEvents}>Planlagte</Button>
-                                <Button name="planning" variant="secondary" active={this.state.active === "planning"} onClick={this.filterEvents}>Under planlegging</Button>
-                                <Button name="archived" variant="secondary" active={this.state.active === "archived"} onClick={this.filterEvents}>Arkiverte</Button>
-                            </ButtonGroup>
+                    <Col>
+                        <ButtonGroup size="sm">
+                            <Button name="all" variant="secondary" active={this.state.active === "all"}
+                                    onClick={this.filterEvents}>Alle</Button>
+                            <Button name="planning" variant="secondary" active={this.state.active === "planning"}
+                                    onClick={this.filterEvents}>Under Planlegging</Button>
+                            <Button name="published" variant="secondary" active={this.state.active === "published"}
+                                    onClick={this.filterEvents}>Publisert</Button>
+                            <Button name="archived" variant="secondary" active={this.state.active === "archived"}
+                                    onClick={this.filterEvents}>Arkiverte</Button>
+                            <Button name="cancelled" variant="secondary" active={this.state.active === "cancelled"}
+                                    onClick={this.filterEvents}>Kansellerte</Button>
+                        </ButtonGroup>
 
-                            <DropdownButton size="sm" variant="info" title="Sorter etter..">
-                                <Dropdown.Item as="button">Dato</Dropdown.Item>
-                                <Dropdown.Item as="button">Pris</Dropdown.Item>
-                            </DropdownButton>
-                        </Col>
+                        <DropdownButton size="sm" variant="info" title="Sorter etter..">
+                            <Dropdown.Item as="button">Dato</Dropdown.Item>
+                            <Dropdown.Item as="button">Pris</Dropdown.Item>
+                        </DropdownButton>
+                    </Col>
 
-                    </Row>
+                </Row>
 
-                <Accordion id="plannedEvents" defaultActiveKey="0">
+                <Accordion id="publishedEvents" defaultActiveKey="0">
                     <Row className="no-gutters">
-                        <p>Planlagte arrangement</p>
+                        <p>Publisert</p>
                         <Accordion.Toggle as={FaAngleDown} variant="link" eventKey="0"/>
                     </Row>
                     <Accordion.Collapse eventKey="0">
@@ -100,7 +107,7 @@ export class Dashboard extends React.Component {
                     </Accordion.Collapse>
                 </Accordion>
 
-                <Accordion defaultActiveKey="0">
+                <Accordion id="plannedEvents" defaultActiveKey="0">
                     <Row className="no-gutters">
                         <p>Under planlegging</p>
                         <Accordion.Toggle as={FaAngleDown} variant="link" eventKey="0"/>
@@ -114,9 +121,9 @@ export class Dashboard extends React.Component {
                     </Accordion.Collapse>
                 </Accordion>
 
-                <Accordion defaultActiveKey="1">
+                <Accordion id="archivedEvents" defaultActiveKey="1">
                     <Row className="no-gutters">
-                        <p>Arkiverte</p>
+                        <p>Arkivert</p>
                         <Accordion.Toggle as={FaAngleDown} variant="link" eventKey="0"/>
                     </Row>
                     <Accordion.Collapse eventKey="0">
@@ -127,18 +134,33 @@ export class Dashboard extends React.Component {
                         </Row>
                     </Accordion.Collapse>
                 </Accordion>
+
+                <Accordion id="cancelledEvents" defaultActiveKey="1">
+                    <Row className="no-gutters">
+                        <p>Kansellert</p>
+                        <Accordion.Toggle as={FaAngleDown} variant="link" eventKey="0"/>
+                    </Row>
+                    <Accordion.Collapse eventKey="0">
+                        <Row className="no-gutters">
+                            {this.state.events.filter(e => e.status === 3).length > 0 ?
+                                <EventView events={this.state.events.filter(event => event.status === 3)}/> :
+                                <NoEvents message="Du har ingen kansellerte arrangement"/>}
+                        </Row>
+                    </Accordion.Collapse>
+                </Accordion>
+
                 <Row>
-                   <Col className="pull-right" size={12}>
-                       <div onClick={this.addEventClicked} align="right">
-                           <FaPlusCircle className="ml-2" size={60}/>
-                       </div>
-                   </Col>
+                    <Col className="pull-right" size={12}>
+                        <div onClick={this.addEventClicked} align="right">
+                            <FaPlusCircle className="ml-2" size={60}/>
+                        </div>
+                    </Col>
                 </Row>
             </Card>
         )
     }
 
-    searchHandler(){
+    searchHandler() {
 
     }
 }
