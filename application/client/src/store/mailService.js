199353aa0e42e3f5044f63fff7aeacd69905d587
LAPTOP-6SJOR7OT\Even <evengu@stud.ntnu.no>
2020-01-20 08:57:10 +0100

    Finished mail service for sending cancel notice

--- application/client/src/store/mailService.js
+++ application/client/src/store/mailService.js
@@ -27,21 +27,20 @@ export class MailService{
         }).catch(err => callback(500, err));
     }
 
+    static sendGeneralEmailToOne(emailTo, subject, emailBody, attachmentLinks, callback){
+        this.sendGeneralEmail(emailTo, subject, emailBody, attachmentLinks, callback);
+    }
+
     static sendGeneralEmailToMany(emailToArray, subject, emailBody, attachmentLinks, callback){
-        let header = {
-            "Content-Type": "application/json",
-            "x-access-token": CookieStore.currentToken
-        };
-
-        let body = {
-
-        }
+        this.sendGeneralEmail(emailToArray, subject, emailBody, attachmentLinks, callback);
     }
 
     static sendCancelNotice(subject, emailBody, artists, crew, otherEmails, callback){
         let artistEmails = artists.map(artist => artist.email);
         let crewEmails = crew.map(crewMember => crewMember.email);
         let allEmails = [].concat(artistEmails).concat(crewEmails.concat(otherEmails));
+
+        this.sendGeneralEmailToMany(allEmails, subject, emailBody, null, callback);
     }
 
     static sendArtistInvitation(artist, subject, emailBody, callback){
