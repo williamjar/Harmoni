da4e167493b16ef7c37f93161730fc55a0f6e4bb
Joakim Moe Adolfsen <joakimad@stud.ntnu.no>
2020-01-23 11:24:47 +0100

    removed more comments and simplified

--- application/client/src/components/content/tabContent.js
+++ application/client/src/components/content/tabContent.js
@@ -30,10 +30,7 @@ export class TabContent extends Component {
                 <Row className="event-buttons">
                     <Col>
                         <div>
-                            <Button hidden={(this.state.activeTab === 3)} className="float-right mr-1" onClick={() => {
-                                console.log("next");
-                                this.props.btnClick();
-                            }}>Neste</Button>
+                            <Button hidden={(this.state.activeTab === 3)} className="float-right mr-1" onClick={this.props.btnClick()}>Neste</Button>
 
                             <Button hidden={!(this.state.status === 1)} variant="danger" className="mr-2" onClick={() => {
                                 if (window.confirm('Er du sikker på at du vil kansellere dette arrangementet?')) this.cancelEvent();
