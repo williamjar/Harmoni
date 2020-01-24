c9881fbc6b2083de1bec5bf557d6219728bcc33c
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-24 08:57:56 +0100

    JSDocs

--- application/client/src/classes/artist.js
+++ application/client/src/classes/artist.js
@@ -3,8 +3,24 @@ import {Organizer} from "./organizer";
 import {Document} from "./document";
 import {Genre} from "./genre";
 
+/**
+ * @class {Artist}
+ * This is an object for storing all the data required to define an artist
+ */
 export class Artist extends Contact {
 
+    /**
+     *
+     * @param {int} artistID
+     * @param {int} contactID
+     * @param {String} name
+     * @param {String} phone
+     * @param {String} email
+     * @param {Genre} genre
+     * @param {Organizer} organizer
+     * @param {boolean} hasBeenPaid
+     * @param {boolean} contractSigned
+     */
     constructor(artistID, contactID, name, phone, email, genre, organizer, hasBeenPaid, contractSigned) {
         super(contactID, name, phone, email);
         this.artistID = artistID;
