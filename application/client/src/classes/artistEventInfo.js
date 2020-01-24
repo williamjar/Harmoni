272f6c9bfe1c2271421f66282a18b76355251a73
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-24 09:04:23 +0100

    ArtistEventInfo JSDocs

--- application/client/src/classes/artistEventInfo.js
+++ application/client/src/classes/artistEventInfo.js
@@ -1,5 +1,17 @@
+/**
+ * @class {ArtistEventInfo}
+ * A class for storing information relating to a artist connected to an event
+ * This class contains information on whether the artist has been paid and if the artist's contract has been signed
+ */
 export class ArtistEventInfo {
 
+    /**
+     *
+     * @param {int} artistID
+     * @param {int} eventID
+     * @param {boolean} contractSigned
+     * @param {boolean} hasBeenPaid
+     */
     constructor(artistID, eventID, contractSigned, hasBeenPaid){
         this.artistID = artistID;
         this.eventID = eventID;
