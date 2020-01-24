3879972da94c8afd232491b1fbda41f49061d637
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-24 15:40:15 +0100

    JSDocs bugDao

--- application/server/src/dao/bugDao.js
+++ application/server/src/dao/bugDao.js
@@ -4,16 +4,38 @@ module.exports = class bugDao extends Dao {
 
 
     //Register bug
+    /**
+     * Register a bug
+     * @param {function} callback
+     * gets passed on to the main DAO
+     * @see Dao
+     * @param {Array}list
+     * list of parameters [description , organizerID]
+     */
     registerBug(callback, list){
         super.query('INSERT INTO bug (date , description , organizerID, resolved) VALUES (CURRENT_DATE, ?, ?, 0)', list , callback);
     }
 
-
+    /**
+     * Get all bugs reported by one user (organizer)
+     * @param {function} callback
+     * gets passed on to the main DAO
+     * @see Dao
+     * @param {int}organizerID
+     * The ID of the organizer.
+     */
     getAllBugsFromOrganizer(callback, organizerID){
         super.query('SELECT * FROM bug WHERE organizerID = ?', [organizerID], callback);
     }
 
-
+    /**
+     * Gat a bug
+     * @param {function} callback
+     * gets passed on to the main DAO
+     * @see Dao
+     * @param {int}bugID
+     * The ID of the bug.
+     */
     getOneBug(callback, bugID){
         super.query('SELECT * FROM bug WHERE bugID = ?', [bugID], callback);
     }
@@ -22,6 +44,14 @@ module.exports = class bugDao extends Dao {
         super.query('SELECT * FROM bug', callback);
     }
 
+    /**
+     * Delete a bug
+     * @param {function} callback
+     * gets passed on to the main DAO
+     * @see Dao
+     * @param {int}bugID
+     * The ID of the bug.
+     */
     deleteBug(callback, bugID) {
         super.query('DELETE FROM bug WHERE bugID = ?', [bugID], callback);
     }
