939e5312d3d6b271c00d71420b97dd247755dd30
marcuse <marcuse@stud.ntnu.no>
2020-01-24 16:34:07 +0100

    Added limitation to events on dashboard and fixed default sorting bug

--- application/client/src/components/content/dashboard/eventView.js
+++ application/client/src/components/content/dashboard/eventView.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from "react-bootstrap/Table";
 import {EventCard} from "./eventCard";
 import {Card, CardGroup, CardStock, ListGroup} from "react-bootstrap";
+import {FaAngleDown} from "react-icons/all";
 
 // Component for displaying a group of events(EventCard's) as a table
 export class EventView extends React.Component {
 
     state = {
-        events: [this.props.events]
+        events: [this.props.events],
+        limitation: 5,
     };
 
     // Updates the state if the received props from parent is changed
@@ -22,12 +24,25 @@ export class EventView extends React.Component {
         return null;
     }
 
+    showMore = () => {
+        this.setState({limitation: this.state.limitation + 5})
+    };
+
     render() {
         return(
             <Table>
-                {this.state.events.map(event => (
+                {this.state.events.length <= this.state.limitation ? this.state.events.map(event => (
                     <EventCard key={event.eventID} event={event}/>
-                ))}
+                )) :
+                    <div>
+                        {this.state.events.slice(0, this.state.limitation).map(event => (
+                            <EventCard key={event.eventID} event={event}/>
+                            ))}
+                            <div className="text-center showMore pointer" onClick={this.showMore}>
+                                Vis flere
+                                <FaAngleDown size={20}/>
+                            </div>
+                    </div>}
             </Table>
         )
     }
