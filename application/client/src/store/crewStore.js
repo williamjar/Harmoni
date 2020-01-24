a572d9ed37cc2959e073e5a29402223439dc45a6
andinel <andine.luick@gmail.com>
2020-01-24 17:53:26 +0100

    fixed array-reset

--- application/client/src/store/crewStore.js
+++ application/client/src/store/crewStore.js
@@ -46,17 +46,15 @@ export class CrewStore {
      */
     static storeAllCrewMembersForOrganizer(callback, organizerID) {
 
-        this.allCrewMembersForOrganizer = [];
-
         let header = {
             "Content-Type": "application/json",
             "x-access-token": CookieStore.currentToken
         };
 
         axios.get(axiosConfig.root + '/api/crew/organizer/' + organizerID, {headers: header}).then(response => {
+            this.allCrewMembersForOrganizer = [];
 
             response.data.map(data => {
-
                 this.allCrewMembersForOrganizer.push(new CrewMember(data.crewID, data.contactID, data.description, data.crewCategoryID,
                     data.crewCategoryName, data.contactName, data.phone, data.email));
 
@@ -72,7 +70,6 @@ export class CrewStore {
      * @param {int} eventID - The database ID of the event.
      */
     static storeAllCrewMembersForEvent(callback, eventID) {
-        this.allCrewForCurrentEvent = [];
 
         let header = {
             "Content-Type": "application/json",
@@ -80,6 +77,7 @@ export class CrewStore {
         };
 
         axios.get(axiosConfig.root + '/api/crew/event/' + eventID, {headers: header}).then(response => {
+            this.allCrewForCurrentEvent = [];
 
             response.data.map(data => {
                 this.allCrewForCurrentEvent.push(new CrewMember(data.crewID, data.contactID, data.description, data.crewCategoryID,
@@ -96,7 +94,7 @@ export class CrewStore {
      */
     static storeAllCrewCategoriesForOrganizer(callback, organizerID) {
 
-        this.allCrewCategoriesForOrganizer = [];
+
 
         let header = {
             "Content-Type": "application/json",
@@ -104,6 +102,7 @@ export class CrewStore {
         };
 
         axios.get(axiosConfig.root + '/api/crew/categories/' + organizerID, {headers: header}).then(response => {
+            this.allCrewCategoriesForOrganizer = [];
 
             response.data.map(data => {
                 this.allCrewCategoriesForOrganizer.push(new CrewCategory (data.crewCategoryID, data.crewCategoryName));
@@ -120,17 +119,15 @@ export class CrewStore {
      */
     static storeAllCrewCategoriesForEvent(callback, eventID) {
 
-        this.allCrewCategoriesForCurrentEvent = [];
-
         let header = {
             "Content-Type": "application/json",
             "x-access-token": CookieStore.currentToken
         };
 
         axios.get(axiosConfig.root + '/api/crew/event/' + eventID + '/categories', {headers: header}).then(response =>  {
+            this.allCrewCategoriesForCurrentEvent = [];
 
             response.data.map(data => {
-
                 this.allCrewCategoriesForCurrentEvent.push(new CrewCategory (data.crewCategoryID, data.crewCategoryName));
 
             });
