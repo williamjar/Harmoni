811a708128d3722a1d364198208f530909f4947a
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2020-01-24 17:55:27 +0100

    JSDocs dao

--- application/server/src/dao/dao.js
+++ application/server/src/dao/dao.js
@@ -2,6 +2,17 @@ module.exports = class Dao {
     constructor(pool) {
         this.pool = pool;
     }
+
+    /**
+     * Main method for querying the sql database.
+     * @param {String}sql
+     * The SQL string that gets queried.
+     * @param {Array}params
+     * Array of parameters for the query.
+     * @param {function} callback
+     * This function should contain what will happen with the results of the query.
+     * This method (query) feeds inn a status and the SQL result as parameters.
+     */
     query(sql, params, callback) {
         console.log("DAO PARAMS " + params);
         this.pool.getConnection((err, connection) => {
