00de8e4c1084af6bec0d0b5328d68a2ba1614df5
Jørgen Hollum <jorho@stud.ntnu.no>
2020-01-24 19:21:31 +0100

    Commenting documentationdao.js

--- application/server/src/dao/documentationdao.js
+++ application/server/src/dao/documentationdao.js
@@ -1,6 +1,10 @@
 const Dao = require("./dao.js");
 
 //Sql queries for associated with Documentation
+/**
+ * Methods to contact db with execution commands
+ * @class {documentationDao}
+ */
 module.exports = class documentationDao extends Dao {
     getAllDocumentCategories(callback){
         super.query(
@@ -10,6 +14,16 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
+    /**
+     * Insert document to db
+     * @function
+     * @param {int} eventID - id to event
+     * @param {string} documentName - name of document
+     * @param {string} link - file path on server
+     * @param {int} artistID - id of artist
+     * @param {int} crewID - id of a crew member
+     * @param {int} categoryID - id of a category
+     */
     insertDocument(eventID, documentName, link, artistID, crewID, categoryID, callback){
         console.log("Inserting file on " + link);
         let val = [eventID, documentName, link, artistID, crewID, categoryID];
@@ -21,7 +35,11 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
-
+    /**
+     * Get all documents from db
+     * @param {int} eventID - id of event
+     * @param {function} callback - handle response
+     */
     getAllDocuments(eventID, callback){
         super.query(
             "select * from document where eventID = ?",
@@ -30,7 +48,12 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
-
+    /**
+     * Get all documents by category
+     * @param {int} eventID - id of event
+     * @param {int} documentCategoryID - id of document category
+     * @param {function} callback - handle response
+     */
     getDocumentsByCategory(eventID, documentCategoryID, callback){
         const val = [eventID, documentCategoryID];
         super.query(
@@ -40,6 +63,13 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
+    /**
+     * Change category of document
+     * @param {int} eventID - id of event
+     * @param {int} documentCategoryID - id of document category
+     * @param {object} json -
+     * @param {function} callback - handle response
+     */
     changeDocumentCategory(eventID, documentCategoryID, json, callback){
         const val = [eventID, documentCategoryID, json.documentID];
         super.query(
@@ -49,6 +79,12 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
+    /**
+     * Delete a document
+     * @param {int} eventID - id of event
+     * @param {int} documentID - id of document
+     * @param {function} callback - handle response
+     */
     deleteDocument(eventID, documentID, callback){
         const val = [eventID, documentID];
         super.query(
@@ -58,7 +94,11 @@ module.exports = class documentationDao extends Dao {
         );
     }
 
-
+    /**
+     * Get all document categories for event
+     * @param {int} eventID - id of event
+     * @param {function} callback - handle response
+     */
     getAllDocumentCategoriesForEvent(eventID, callback){
         const val = [eventID];
         super.query("SELECT DISTINCT documentCategory.documentCategoryID, documentCategory.documentCategoryName from documentCategory left join document on documentCategory.documentCategoryID = document.documentCategoryID where document.eventID = ?",
@@ -66,24 +106,44 @@ module.exports = class documentationDao extends Dao {
     }
 
 
-
+    /**
+     * Get all documents by category for event
+     * @param {int} eventID - id of event
+     * @param {int} documentCategoryID - id of document category
+     * @param {function} callback - handle response
+     */
     getAllDocumentsByCategoryForEvent(eventID,documentCategoryID, callback){
         const val = [eventID, documentCategoryID];
         super.query("SELECT * from document where eventID = ? and documentCategoryID = ?",
             val, callback);
     }
 
-
+    /**
+     * Get all documents for artist
+     * @param {int} eventID - id of event
+     * @param {int} artistID - id of artist
+     * @param {function} callback - handle response
+     */
     getDocumentsForArtist(eventID, artistID, callback){
         super.query("SELECT * FROM document where eventID = ? and artistID = ?", [eventID, artistID], callback);
     }
 
+    /**
+     * Get contact info of artist connected to document
+     * @param {int} documentID - id of document
+     * @param {function} callback - handle response
+     */
     getArtistInfoConnectedToDocument(documentID, callback){
         const val = [documentID];
         super.query("SELECT contact.contactID, contact.contactName, contact.phone, contact.email from contact join artist on contact.contactID = artist.contactID join document on artist.artistID = document.artistID where document.documentID = ?",
             val, callback);
     }
 
+    /**
+     * Get contact info of crew member connected to document
+     * @param {int} documentID - id of document
+     * @param {function} callback - handle response
+     */
     getCrewInfoConnectedToDocument(documentID, callback){
         const val = [documentID];
         super.query("SELECT contact.contactID, contact.contactName, contact.phone, contact.email from contact join crew on contact.contactID = crew.contactID join document on crew.crewID = document.crewID where document.documentID = ?",
